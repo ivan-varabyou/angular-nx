20dbf7cc7e029dd086add63e98c27559b1ab8065
Ivan Varabyou <onebelbiz@gmail.com>
2024-06-27 13:35:40 +0300

    Initial commit

---
+++ apps/frontend-client-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run frontend-client:serve:development',
+        production: 'nx run frontend-client:serve:production',
+      },
+      ciWebServerCommand: 'nx run frontend-client:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});

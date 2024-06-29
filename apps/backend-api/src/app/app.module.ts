cdb7c74dc349c792903a8aa12aa470669a6f2628
Ivan Varabyou <onebelbiz@gmail.com>
2024-06-29 20:12:17 +0300

    feat(api db): enviroment api
    * enviroment for backend-api
    
    #5

--- apps/backend-api/src/app/app.module.ts
+++ apps/backend-api/src/app/app.module.ts
@@ -2,9 +2,14 @@ import {Module} from '@nestjs/common';
 
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
+import {TypeOrmModule, TypeOrmModuleOptions} from "@nestjs/typeorm";
+import {environment} from "../../environments/environment";
+
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    ...environment.connection as TypeOrmModuleOptions
+  })],
   controllers: [AppController],
   providers: [AppService],
 })

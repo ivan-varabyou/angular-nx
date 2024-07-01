f4bcf4f557009c44bc21f02fe5777f75c947d2e0
Ivan Varabyou <onebelbiz@gmail.com>
2024-07-01 18:27:54 +0300

    feat(style): set import to app
    * set import to app
    
    Signed-off-by: Ivan Varabyou <onebelbiz@gmail.com>
    
    #12

--- apps/frontend-client/src/app/app.component.ts
+++ apps/frontend-client/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import {Component} from '@angular/core'
 import {RouterModule} from '@angular/router'
-
+import {MatCardModule} from '@angular/material/card'
+import {MatIconModule} from '@angular/material/icon'
+import {MatButtonModule} from "@angular/material/button";
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatCardModule, MatIconModule, MatButtonModule, MatDividerModule, MatSlideToggleModule, MatSidenav, MatSidenavContent, MatToolbar, MatSidenavContainer],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'

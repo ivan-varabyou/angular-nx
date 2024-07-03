3665ac1ae4326a3ef5968628938f0ba175555bf5
Ivan Varabyou <onebelbiz@gmail.com>
2024-07-03 14:48:47 +0300

    feat(style): add style management
    * add styles material ui
    * add styles bootstrap
    * add library frontend-client-ui-layout
    
    Closes #12
    
    Signed-off-by: Ivan Varabyou <onebelbiz@gmail.com>

--- apps/frontend-client/src/app/app.component.ts
+++ apps/frontend-client/src/app/app.component.ts
@@ -1,20 +1,13 @@
-import {Component} from '@angular/core'
-import {RouterModule} from '@angular/router'
-import {MatCardModule} from '@angular/material/card'
-import {MatIconModule} from '@angular/material/icon'
-import {MatButtonModule} from "@angular/material/button";
-import {MatDividerModule} from '@angular/material/divider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
-import {MatToolbar} from "@angular/material/toolbar";
+import {Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatCardModule, MatIconModule, MatButtonModule, MatDividerModule, MatSlideToggleModule, MatSidenav, MatSidenavContent, MatToolbar, MatSidenavContainer],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend-client'
+  title = 'frontend-client';
 }

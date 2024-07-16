07e3ec9b175b1050b2711abd9598982cc89872b9
Ivan Varabyou <onebelbiz@gmail.com>
2024-07-16 13:00:13 +0300

    feat(lib*interfaces): add data-access lib:interfaces [WiP]
    * add data-access lib:interfaces
    
    Signed-off-by: Ivan Varabyou <onebelbiz@gmail.com>
    
    #12

---
+++ libs/shared/data-access/interfaces/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/shared-data-access-interfaces/shared-data-access-interfaces.component';
---
+++ libs/shared/data-access/interfaces/src/lib/shared-data-access-interfaces/shared-data-access-interfaces.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+    selector: 'lib-shared-data-access-interfaces',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './shared-data-access-interfaces.component.html',
+    styleUrl: './shared-data-access-interfaces.component.css',
+})
+export class SharedDataAccessInterfacesComponent {
+}

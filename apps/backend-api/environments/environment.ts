f659aac945f9c5a9dff8ae317ca6f0ed776bae31
Ivan Varabyou <onebelbiz@gmail.com>
2024-07-25 17:14:57 +0300

    feat(api-auth): add auth module [WiP]
    * add auth.services.ts => jwt section
    * change environment.ts
    * add jwt.strategy.ts
    * add .env jwt options
    
    Signed-off-by: Ivan Varabyou <onebelbiz@gmail.com>
    
    #12

--- apps/backend-api/environments/environment.ts
+++ apps/backend-api/environments/environment.ts
@@ -1,23 +1,29 @@
+import * as process from "node:process";
+
 export const environment = {
-  production: false,
-  connection: {
-    type: process.env.DB_TYPE,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER_NAME,
-    password: process.env.DB_USER_PASSWORD,
-    database: process.env.DB_NAME,
-    dropSchema: false,
-    synchronize: true,
-    logging: true, // log db in console
-    entities: [
-      'dist/apps/backend/api/src/**/*.entity{.ts, .js}'
-    ],
-    migrations: [
-      'dist/apps/backend/api/migrations/*{.ts, .js}'
-    ],
-    cli: {
-      'migrationsDir': 'db/migrations'
+    production: false,
+    jwt: {
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRES_IN
+    },
+    connection: {
+        type: process.env.DB_TYPE,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER_NAME,
+        password: process.env.DB_USER_PASSWORD,
+        database: process.env.DB_NAME,
+        dropSchema: false,
+        synchronize: true,
+        logging: true, // log db in console
+        entities: [
+            'dist/apps/backend/api/src/**/*.entity{.ts, .js}'
+        ],
+        migrations: [
+            'dist/apps/backend/api/migrations/*{.ts, .js}'
+        ],
+        cli: {
+            'migrationsDir': 'db/migrations'
+        }
     }
-  }
 };
--- apps/backend-api/src/app/auth/services/auth.services.ts
+++ apps/backend-api/src/app/auth/services/auth.services.ts
@@ -1,13 +1,20 @@
 import {Injectable} from "@nestjs/common";
 import {ISignAuthPayload, ISignAuthResponse} from "@angular-nx/shared/data-access/interfaces";
+import {UserService} from "../../users/services/user.service";
 
 @Injectable()
 export class AuthServices {
+    constructor(private readonly userService: UserService) {
+    }
+
     async login(authServicesPayload: ISignAuthPayload): Promise<ISignAuthResponse> {
-        return {
-            accessToken: '44444',
-            expiresIn: 3600,
-            id: 1
-        }
+
+
+        return
+        // {
+        //     accessToken: '44444',
+        //     expiresIn: 3600,
+        //     id: this.userService.findOneById()
+        // }
     }
 }

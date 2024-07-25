923d7cf6ff4610dfa0a90a7e7fffcabcb34dd81d
Ivan Varabyou <onebelbiz@gmail.com>
2024-07-25 15:18:35 +0300

    feat(api-auth*shared-data-access-interfaces): add auth module [WiP]
    * add auth.module => backend-api
      * add auth.module.ts
      * add auth.graphql
      * change users.graphql
      * change graphql.config.yml
      * add signin.auth.interfaces
    
    Signed-off-by: Ivan Varabyou <onebelbiz@gmail.com>
    
    #12

--- apps/backend-api/src/app/app.module.ts
+++ apps/backend-api/src/app/app.module.ts
@@ -1,33 +1,32 @@
 import {Module} from '@nestjs/common';
 
 import {AppController} from './app.controller';
-import {TypeOrmModule, TypeOrmModuleOptions} from "@nestjs/typeorm";
-import {environment} from "../../environments/environment";
-import {GraphQLModule} from "@nestjs/graphql";
-import {resolverMap} from "./app.resolver";
+import {TypeOrmModule, TypeOrmModuleOptions} from '@nestjs/typeorm';
+import {environment} from '../../environments/environment';
+import {GraphQLModule} from '@nestjs/graphql';
+import {resolverMap} from './app.resolver';
 import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo';
-import {UserEntitiy} from "./users/entities/user.entitiy";
-import {UsersModule} from "./users/users.module";
+import {UserEntitiy} from './users/entities/user.entitiy';
+import {UsersModule} from './users/users.module';
+import {AuthModule} from './auth/auth.module';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot({
             ...environment.connection,
-            entities: [UserEntitiy]
+            entities: [UserEntitiy],
         } as TypeOrmModuleOptions),
-        GraphQLModule.forRoot<ApolloDriverConfig>(
-            {
-                driver: ApolloDriver,
-                typePaths: ["./**/*.graphql"],
-                context: ({req}) => ({req}),
-                playground: true,
-                resolvers: [resolverMap]
-            }
-        ),
-        UsersModule
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            typePaths: ['./**/*.graphql'],
+            context: ({req}) => ({req}),
+            playground: true,
+            resolvers: [resolverMap],
+        }),
+        UsersModule,
+        AuthModule,
     ],
     controllers: [AppController],
-
 })
 export class AppModule {
 }

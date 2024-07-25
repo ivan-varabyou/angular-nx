8d9d5362103731b29837cc49a1dacba36d59b017
Ivan Varabyou <onebelbiz@gmail.com>
2024-07-25 13:18:49 +0300

    feat(api-users): add users module [WiP]
    * add users.module.ts => backend-api
    
    Signed-off-by: Ivan Varabyou <onebelbiz@gmail.com>
    
    #12

---
+++ apps/backend-api/src/app/users/resolvers/user.resolver.ts
@@ -0,0 +1,14 @@
+import {Resolver} from "@nestjs/graphql";
+import {CurrentUser} from "../decorators/user.decorator";
+import {UserEntitiy} from "../entities/user.entitiy";
+import {UserService} from "../services/user.service";
+
+@Resolver("User")
+export class UserResolver {
+    constructor(private readonly userService: UserService) {
+    }
+
+    async whoAmI(@CurrentUser() user: UserEntitiy) {
+        return this.userService.findOneById(user.id)
+    }
+}

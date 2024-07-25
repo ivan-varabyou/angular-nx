16ed77475323c2f7b2c048274590e77a18bb4cfa
Ivan Varabyou <onebelbiz@gmail.com>
2024-07-25 17:41:02 +0300

    feat(api-auth): add auth module [WiP]
    * change auth.services.ts
    
    Signed-off-by: Ivan Varabyou <onebelbiz@gmail.com>
    
    #12

--- apps/backend-api/src/app/auth/services/auth.services.ts
+++ apps/backend-api/src/app/auth/services/auth.services.ts
@@ -1,20 +1,42 @@
-import {Injectable} from "@nestjs/common";
+import {Injectable, UnauthorizedException} from "@nestjs/common";
 import {ISignAuthPayload, ISignAuthResponse} from "@angular-nx/shared/data-access/interfaces";
 import {UserService} from "../../users/services/user.service";
+import {JwtService} from "@nestjs/jwt";
+import {PasswordService} from "./password.service";
+import {UserEntitiy} from "../../users/entities/user.entitiy";
+import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class AuthServices {
-    constructor(private readonly userService: UserService) {
+    constructor(
+        private readonly passwordService: PasswordService,
+        private readonly jwtService: JwtService,
+        private readonly userService: UserService
+    ) {
     }
 
-    async login(authServicesPayload: ISignAuthPayload): Promise<ISignAuthResponse> {
+    async validateUser(username: string, pass: string): Promise<Omit<UserEntitiy, 'password'>> {
+        const user = await this.userService.findOneByUserName(username)
 
+        const isValid = user ? await this.passwordService.compareHash(pass, user.password) : false
+        if (isValid) {
+            delete user.password
+        }
 
-        return
-        // {
-        //     accessToken: '44444',
-        //     expiresIn: 3600,
-        //     id: this.userService.findOneById()
-        // }
+        return user ?? null
+    }
+
+    async login(signInPayload: ISignAuthPayload): Promise<ISignAuthResponse> {
+        const user = await this.validateUser(signInPayload.username, signInPayload.password)
+        if (!user) {
+            throw new UnauthorizedException()
+        }
+        const payload = {username: user.username, userid: user.id}
+
+        return {
+            accessToken: this.jwtService.sign(payload),
+            expiresIn: new Date(environment.jwt.expiresIn).getDate(),
+            id: user.id
+        }
     }
 }

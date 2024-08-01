688dda3689658661f8a7a28ba7d0247383336d36
Ivan Varabyou <onebelbiz@gmail.com>
2024-08-01 13:12:59 +0300

    feat(ws*frontend-client): change app.config.server.ts [WiP]
    Signed-off-by: Ivan Varabyou <onebelbiz@gmail.com>
    
    #29

--- apps/frontend-client/src/app/app.config.server.ts
+++ apps/frontend-client/src/app/app.config.server.ts
@@ -1,9 +1,25 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core'
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core'
 import { provideServerRendering } from '@angular/platform-server'
 import { appConfig } from './app.config'
+import { provideRouter } from '@angular/router'
+import { appRoutes } from './app.routes'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import {
+	provideClientHydration,
+	withEventReplay
+} from '@angular/platform-browser'
+import { provideHttpClient, withFetch } from '@angular/common/http'
 
 const serverConfig: ApplicationConfig = {
-	providers: [provideServerRendering()]
+	providers: [
+		provideServerRendering(),
+		provideRouter(appRoutes),
+		provideAnimationsAsync(),
+		provideHttpClient(withFetch()),
+		provideClientHydration(
+			withEventReplay()
+		)
+	]
 }
 
 export const config = mergeApplicationConfig(appConfig, serverConfig)

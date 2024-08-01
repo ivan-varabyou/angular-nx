ab89795d55bf9cabf309bbe1975863d93a51fddd
Ivan Varabyou <onebelbiz@gmail.com>
2024-08-01 14:14:15 +0300

    feat(ws*frontend-admin): add ssr for frontend-admin [WiP]
    Signed-off-by: Ivan Varabyou <onebelbiz@gmail.com>
    
    #29

--- apps/frontend-client/src/app/app.config.ts
+++ apps/frontend-client/src/app/app.config.ts
@@ -1,11 +1,32 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
+import {
+	ApplicationConfig,
+	enableProdMode,
+	provideZoneChangeDetection
+} from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { appRoutes } from './app.routes'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideStore } from '@ngrx/store'
+import { provideHttpClient, withFetch } from '@angular/common/http'
+import { ngrxDevtools } from '../devtools/ngrx-devtools'
+import { environment } from '../environments/environment'
+import { ngrxDevtoolsDevelopment } from '../devtools/ngrx-devtools.development'
+
+export const commonProviders = [
+	provideStore(),
+	provideRouter(appRoutes),
+	provideAnimationsAsync(),
+	provideHttpClient(withFetch())
+]
+
+if (environment.production) {
+	enableProdMode()
+}
 
 export const appConfig: ApplicationConfig = {
 	providers: [
+		...commonProviders,
 		provideZoneChangeDetection({eventCoalescing: true}),
-		provideRouter(appRoutes), provideAnimationsAsync()
+		environment.production ? ngrxDevtools : ngrxDevtoolsDevelopment
 	]
 }
